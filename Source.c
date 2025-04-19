64a6745bf56dc12552fa6bfa7e0b93ec29490e05
EMELE WINNER <winnerumaemele2007@gmail.com>
2025-04-19 14:40:17 -0700

    Add project files.

---
+++ Source.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+// to create the grid layout for the tic tac toe.
+char matrix[3][3] = {
+    {'-', '-', '-'},
+    {'-', '-', '-'},
+    {'-', '-', '-'},
+
+};
+void firsttable() {
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+
+            printf("%c\t|\t", matrix[row][col]);
+        }
+        printf("\n\n");
+    }
+}
+void initializegame(int rowan, int columnan, char var) {
+    char* ptr = &matrix[rowan][columnan];
+    *ptr = var;
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+
+            printf("%c\t|\t", matrix[row][col]);
+        }
+        printf("\n\n");
+    }
+}
+int main() {
+    printf("tictactoe-game\n");
+    firsttable();
+    int max_plays = 9;
+    int rowa, columna;
+    for (int play = 0; play < max_plays; play++) {
+        if (play % 2 == 0) {
+            printf("player x's turn\nselect an empty row and column:\n");
+            scanf_s("%d\n", &rowa);
+            scanf_s("%d", &columna);
+            matrix[rowa][columna] = 'x';
+            initializegame(rowa, columna, 'x');
+
+        }
+        else {
+            printf("player o's turn\nselect an empty row and column:");
+            scanf_s("%i\n", &rowa);
+            scanf_s("%i", &columna);
+            matrix[rowa][columna] = 'o';
+            initializegame(rowa, columna, 'o');
+        }
+    }
+}
